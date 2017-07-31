c695bb620a70ec918a19e2fa8b2d2b7cabbc956c
Fernando <fbirarra@uc.cl>
2017-07-31 19:42:18 -0400

    Arreglado scrape cuotas

--- scrapeCuotas.py
+++ scrapeCuotas.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import and_
 import mechanicalsoup
 import locale
-from datetime import datetime
+from datetime import datetime, timedelta
 
 fondos = ["A", "B", "C", "D", "E"]
 
@@ -34,7 +34,10 @@ try:
 
         print("Fecha disponible: "+str(fecha_str))
         fecha = datetime.strptime(fecha_str, '%d-%B-%Y').date()
-        
+        dia_de_la_semana = fecha.weekday()
+        dia_anterior = fecha - timedelta(days=1)
+        dia_antesdeayer = fecha - timedelta(days=2)
+
         for afp in afps:
             closest_cuota = Cuota.query.filter(
                       and_(Cuota.AFP_id == afp.id, Cuota.fondo == f)).order_by(Cuota.fecha.desc()).first()
@@ -45,18 +48,34 @@ try:
             if closest_cuota.fecha < fechaDisponible:
                 indice_cuota = (afp.id - 1) * 2
 
+                finde = False
+
                 try:
                     valorCuota = float(cuotas[indice_cuota].get_text().replace(".", "").replace(",", "."))
                 except ValueError:
                     print("Not a float")
                     continue
 
-                cuota_a_guardar = Cuota(fecha=fecha,AFP_id=afp.id,valor=valorCuota,fondo=f) 
-                db.session.add(cuota_a_guardar)
+                if(dia_de_la_semana == 5 and closest_cuota.fecha < dia_anterior):
+                    # Viernes
+                    cuota_a_guardar = Cuota(fecha=dia_anterior,AFP_id=afp.id,valor=valorCuota,fondo=f)
+                    db.session.add(cuota_a_guardar)
+                    finde = True
+
+                elif(dia_de_la_semana == 6 and closest_cuota.fecha < dia_antesdeayer):
+                    # Viernes
+                    cuota_a_guardar = Cuota(fecha=dia_antesdeayer,AFP_id=afp.id,valor=valorCuota,fondo=f)
+                    db.session.add(cuota_a_guardar)
+                    finde = True
+                else:
+                    cuota_a_guardar = Cuota(fecha=fecha,AFP_id=afp.id,valor=valorCuota,fondo=f) 
+                    db.session.add(cuota_a_guardar)
 
                 try:
                     db.session.commit()
                     print("Guardado Cuota: "+str(cuota_a_guardar))
+                    if finde:
+                        print("Se guardó viernes desde el finde")
 
                 except Exception as e:
                     print("Duplicado u otro error: "+str(e))
@@ -65,17 +84,40 @@ try:
 
             if closest_patrimonio.fecha < fechaDisponible:
                 indice_patrimonio = (afp.id * 2) - 1
-                valorPatrimonio = int(cuotas[indice_patrimonio].get_text().replace(".",""))
-                patrimonio_a_guardar = Patrimonio(fecha=fecha,AFP_id=afp.id,valor=valorPatrimonio, fondo=f)
-                db.session.add(patrimonio_a_guardar)
-
+                
                 try:
-                    db.session.commit()
-                    print("Guardado Patrimonio: "+str(patrimonio_a_guardar))
-
-                except Exception as e:
-                    print("Duplicado u otro error: "+str(e))
+                    valorPatrimonio = int(cuotas[indice_patrimonio].get_text().replace(".",""))
+                except ValueError:
+                    print("Not a int")
                     continue
+
+                finde = False
+
+                if valorPatrimonio>0:
+                    if(dia_de_la_semana == 5 and closest_patrimonio.fecha < dia_anterior):
+                        # Viernes o Sabado
+                        patrimonio_a_guardar = Patrimonio(fecha=dia_anterior,AFP_id=afp.id,valor=valorPatrimonio, fondo=f)
+                        db.session.add(patrimonio_a_guardar)
+                        finde = True
+
+                    elif(dia_de_la_semana == 6 and closest_patrimonio.fecha < dia_antesdeayer):
+                        # Viernes
+                        patrimonio_a_guardar = Patrimonio(fecha=dia_antesdeayer,AFP_id=afp.id,valor=valorPatrimonio, fondo=f)
+                        db.session.add(patrimonio_a_guardar)
+                        finde = True
+                    else:
+                        patrimonio_a_guardar = Patrimonio(fecha=fecha,AFP_id=afp.id,valor=valorPatrimonio, fondo=f)
+                        db.session.add(patrimonio_a_guardar)
+
+                    try:
+                        db.session.commit()
+                        print("Guardado Patrimonio: "+str(patrimonio_a_guardar))
+                        if finde:
+                            print("Se guardó viernes desde el finde")
+
+                    except Exception as e:
+                        print("Duplicado u otro error: "+str(e))
+                        continue
             # else:
             #     print("Esa fecha ya está: "+str(closest.fecha))
 
