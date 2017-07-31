7d72c21cd0a1496df75f7e17749392316e34631d
Fernando <fbirarra@uc.cl>
2017-07-31 17:35:59 -0400

    Agregado loggin de errores

--- app/__init__.py
+++ app/__init__.py
@@ -18,5 +18,14 @@ if app.debug:
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
 
+if app.debug is not True:   
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('python.log', maxBytes=1024 * 1024 * 100, backupCount=20)
+    file_handler.setLevel(logging.ERROR)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    file_handler.setFormatter(formatter)
+    app.logger.addHandler(file_handler)
+
 
 
--- app/views.py
+++ app/views.py
@@ -423,11 +423,13 @@ def crearGraficoBarraDesdeDict(titulo, dictionarioDatos):
 
 @app.errorhandler(404)
 def not_found_error(error):
+    app.logger.error(exception)
     return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
+    app.logger.error(error)
     return render_template('500.html'), 500
 
 
